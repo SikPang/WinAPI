e5a10ae93c01cabd8f6ea8e950de1502c1700d80
SikPang <87380790+SikPang@users.noreply.github.com>
2023-03-10 18:40:57 +0900

    03/10 Change_BG

--- WinAPI/Scene.cpp
+++ WinAPI/Scene.cpp
@@ -60,34 +60,6 @@ namespace ks
 					++iter;
 			}
 		}
-
-		// 굳이 vector 만들어서?
-		//std::vector<GameObject*> deleteGameObjects = {};
-		//for (Layer& layer : layers)
-		//{
-		//	std::vector<GameObject*>& gameObjects
-		//		= layer.GetGameObjects();
-
-		//	for (std::vector<GameObject*>::iterator iter = gameObjects.begin()
-		//		; iter != gameObjects.end(); )
-		//	{
-		//		if ((*iter)->GetState() == GameObject::e_State::Death)
-		//		{
-		//			deleteGameObjects.push_back((*iter));
-		//			iter = gameObjects.erase(iter);
-		//		}
-		//		else
-		//		{
-		//			iter++;
-		//		}
-		//	}
-		//}
-
-		//for (GameObject* deathObj : deleteGameObjects)
-		//{
-		//	delete deathObj;
-		//	deathObj = nullptr;
-		//}
 	}
 
 	void Scene::AddGameObject(GameObject* newObject, e_LayerType layer)
